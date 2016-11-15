55d99fa96a8c03479a11e6ecbb5ad550fd1bc5ef
Mirio Eggmann <mirio.eggmann@protonmail.ch>
2016-11-15 09:09:08 +0100

    added angular2-mdl module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { MdlModule } from '../../node_modules/angular2-mdl'
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MdlModule
   ],
   providers: [],
   bootstrap: [AppComponent]
