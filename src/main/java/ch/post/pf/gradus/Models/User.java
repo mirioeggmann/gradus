ca40511a53ffe3764f80af36a81d178c1b0811fb
Manuel Bieri <bierimanu@gmail.com>
2016-12-02 11:54:36 +0100

    Security Startup

--- src/main/java/ch/post/pf/gradus/Models/User.java
+++ src/main/java/ch/post/pf/gradus/Models/User.java
@@ -5,9 +5,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
 @Entity
 @Table (name = "GRAD_user")
-public class User {
+public class User implements UserDetails{
 
     @Id
     @GeneratedValue
@@ -53,8 +58,38 @@ public class User {
         this.email = email;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
     }
 
     public void setPassword(String password) {
